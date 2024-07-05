90fc35cf610937cd397271221b960c6ca5e38fda
noob-master-cell <dheerajkarwasra42@gmail.com>
2024-07-05 12:47:34 +0200

    created test API

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      requierd: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      requierd: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      requierd: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
