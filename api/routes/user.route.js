f556e701a246e4a97941aa2ff149bbaafad682f3
noob-master-cell <dheerajkarwasra42@gmail.com>
2024-07-19 12:37:15 +0200

    Delete account functionality added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,9 @@
 import express from "express"; // Importing Express framework
-import { test, updateUser } from "../controllers/user.controller.js"; // Importing test controller function
+import {
+  test,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.js"; // Importing test controller function
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router(); // Creating a new router instance
@@ -7,5 +11,6 @@ const router = express.Router(); // Creating a new router instance
 // GET /test route handler
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router; // Exporting the router for use in other parts of the application
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Button, TextInput, Alert } from "flowbite-react";
 import {
   getStorage,
@@ -8,6 +8,11 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import app from "/src/firebase.js";
+import {
+  updateFailure,
+  updateStart,
+  updateSuccess,
+} from "../redux/user/userSlice";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
@@ -15,8 +20,11 @@ const DashProfile = () => {
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(0);
   const [imageFileUploadError, setImageFileUploadError] = useState(null);
-  //console.log("Upload progress", imageUploadProgress, imageFileUploadError);
+  const [formData, setFormData] = useState({});
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false); // New state for success alert
+
   const filePickerRef = useRef();
+  const dispatch = useDispatch();
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -33,16 +41,6 @@ const DashProfile = () => {
   }, [imageFile]);
 
   const uploadImage = async () => {
-    // service firebase.storage {
-    //   match /b/{bucket}/o {
-    //     match /{allPaths=**} {
-    //       allow read
-    //       allow write: if
-    //       request.resource.size<2*1024*1024 &&
-    //       request.resource.contentType.matches('image/.*');
-    //     }
-    //   }
-    // }
     const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile.name;
     const storageRef = ref(storage, fileName);
@@ -63,15 +61,47 @@ const DashProfile = () => {
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           setImageFileUrl(downloadURL);
+          setFormData({ ...formData, profilePicture: downloadURL });
         });
       }
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (Object.keys(formData).length === 0) {
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+      } else {
+        dispatch(updateSuccess(data));
+        setShowSuccessAlert(true); // Show success alert
+        setTimeout(() => setShowSuccessAlert(false), 3000); // Hide after 3 seconds
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
           accept="image/*"
@@ -97,14 +127,21 @@ const DashProfile = () => {
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
           type="text"
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
-        <TextInput type="password" id="password" placeholder="password" />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
         <Button
           type="submit"
           className="bg-gradient-to-r from-indigo-400 via-purple-350 to-violet-400 text-white outline-none transition-transform transform hover:scale-105"
@@ -116,6 +153,14 @@ const DashProfile = () => {
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
+      {showSuccessAlert && (
+        <Alert
+          className="flex my-2 justify-center content-center"
+          color="success"
+        >
+          Profile updated successfully
+        </Alert>
+      )}
     </div>
   );
 };
